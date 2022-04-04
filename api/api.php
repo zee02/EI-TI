ff1e3f54cbd17f3f1be91c878709c0f277ed75d4
José Fernandes <zefrancisco2002@gmail.com>
2022-04-04 08:43:24 +0100

    api GET

--- api/api.php
+++ api/api.php
@@ -6,20 +6,22 @@ $hora_temperatura = file_get_contents("/Applications/MAMP/htdocs/EI-TI/api/files
 $log_temperatura = file_get_contents("/Applications/MAMP/htdocs/EI-TI/api/files/temperatura/log.txt");
 $nome_temperatura = file_get_contents("/Applications/MAMP/htdocs/EI-TI/api/files/temperatura/nome.txt");
 
-if($_SERVER['REQUEST_METHOD'] == "POST"){
-print_r($_POST);
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
+    print_r($_POST);
 
-if(isset($_POST["nome"]) && isset($_POST["hora"]) && isset($_POST["valor"])){
-    file_put_contents("/Applications/MAMP/htdocs/EI-TI/api/files/".$_POST['nome']."/hora.txt",$_POST["hora"]);
-    file_put_contents("/Applications/MAMP/htdocs/EI-TI/api/files/".$_POST['nome']."/valor.txt", $_POST["valor"]);
-    file_put_contents("/Applications/MAMP/htdocs/EI-TI/api/files/".$_POST['nome']. "/log.txt", $_POST["hora"].";".$_POST["valor"].PHP_EOL, FILE_APPEND);
-} else{
-    echo "Erro na API";
-}
-}elseif($_SERVER['REQUEST_METHOD'] == "GET"){
-    echo "metodo get";
-}else {
+    if (isset($_POST["nome"]) && isset($_POST["hora"]) && isset($_POST["valor"])) {
+        file_put_contents("/Applications/MAMP/htdocs/EI-TI/api/files/" . $_POST['nome'] . "/hora.txt", $_POST["hora"]);
+        file_put_contents("/Applications/MAMP/htdocs/EI-TI/api/files/" . $_POST['nome'] . "/valor.txt", $_POST["valor"]);
+        file_put_contents("/Applications/MAMP/htdocs/EI-TI/api/files/" . $_POST['nome'] . "/log.txt", $_POST["hora"] . ";" . $_POST["valor"] . PHP_EOL, FILE_APPEND);
+    } else {
+        echo "Erro na API";
+    }
+} elseif ($_SERVER['REQUEST_METHOD'] == "GET") {
+    if (isset($_GET["nome"])) {
+        echo file_get_contents("/Applications/MAMP/htdocs/EI-TI/api/files/" . $_GET['nome'] . "/valor.txt");
+    } else {
+        http_response_code(404);
+    }
+} else {
     echo "metodo errado";
 }
-
-?>
