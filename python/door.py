4818ffaff251dbabe753ffa0b8756ad02a873cce
José Fernandes <zefrancisco2002@gmail.com>
2022-05-30 10:54:24 +0100

    Lab 09 done

--- python/door.py
+++ python/door.py
@@ -18,16 +18,17 @@ def send_to_api( arr ):
         print(r.status_code)
 
 try:
+     print(datahora())
      print("Usage:\n[0]Fecha a porta\n[1]Abre a porta\n[CTRL+C]Terminar")
      while True:
          if getch.getch():
              tecla = getch.getch()
              if tecla == '0':
-                 array = {'nome': 'porta' , 'valor': '0', 'hora': '2021/04/30 23:59:59'}
+                 array = {'nome': 'porta' , 'valor': '0', 'hora': datahora()}
                  send_to_api(array)
                  print("\nA porta foi fechada")
              elif tecla == '1':
-                 array = {'nome': 'porta' , 'valor': '1', 'hora': '2021/04/30 23:59:59'}
+                 array = {'nome': 'porta' , 'valor': '1', 'hora': datahora()}
                  send_to_api(array)
                  print("\nA porta foi aberta")
              else:
